af0c01f142ab1b5b0b98f891b80186b68a67946f
Nosimphiwe <nomsimbenge2@gmail.com>
2020-09-04 20:57:19 +0200

    Created button. Named btnHello with "Hello World" Text. Added messagebox to button click event

--- HelloWorldWinForm/Form1.cs
+++ HelloWorldWinForm/Form1.cs
@@ -16,5 +16,10 @@ namespace HelloWorldWinForm
         {
             InitializeComponent();
         }
+
+        private void btnHello_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World!");
+        }
     }
 }
